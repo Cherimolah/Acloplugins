b4efc0c8953c2d2379db8170b76bccc4236dcb64
Cherimolah <inkustregister@mail.ru>
2022-04-03 16:02:02 +0500

    Добавлен путь к плагинам

--- alcoplugins/urls.py
+++ alcoplugins/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from minecraft_plugins.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('plugins/', index)
 ]
